5998bffc8ddb0e6e401c66cf47e7ea26bd0f6b37
Ryanmeakins <Ryanmeakins@gmail.com>
2018-05-18 12:59:58 -0700

    Added basic student model

---
+++ EverPresent/EverPresent/Models/Enums/StudentStatusEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EverPresent.Models.Enums
+{
+    /// <summary>
+    /// Student Status Options
+    /// </summary>
+    public enum StudentStatusEnum
+    {
+        // Logged Out
+        Out = 1,
+
+        // Logged In
+        In = 2,
+
+        // Student on hold
+        Hold = 3
+    }
+}
---
+++ EverPresent/EverPresent/Models/StudnetModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+
+namespace EverPresent.Models
+{
+    public class StudnetModel
+    {
+        /// <summary>
+        /// The ID for the Student, this is the key, and a required field
+        /// </summary>
+        [Key]
+        [Display(Name = "Id", Description = "Student Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The Friendly name for the student, does not need to be directly associated with the actual student name
+        /// </summary>
+        [Display(Name = "Name", Description = "Nick Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The ID of the Avatar the student is associated with, this will convert to an avatar picture
+        /// </summary>
+        [Display(Name = "AvatarId", Description = "Avatar")]
+        [Required(ErrorMessage = "Avatar is required")]
+        public string AvatarId { get; set; }
+
+        /// <summary>
+        /// The status of the student, for example currently logged in, out
+        /// </summary>
+        [Display(Name = "Current Status", Description = "Status of the Student")]
+        [Required(ErrorMessage = "Status is required")]
+        public StudentStatusEnum Status { get; set; }
+
+
+    }
+}
