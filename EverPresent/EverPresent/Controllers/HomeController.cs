19de7122aa15638bccceceb6202ffdd455b2d9d0
lukekorsman <korsmanl@seattleu.edu>
2018-05-31 09:01:28 -0700

    Removed unused methods. Added comments to Index method.

--- EverPresent/EverPresent/Controllers/HomeController.cs
+++ EverPresent/EverPresent/Controllers/HomeController.cs
@@ -8,21 +8,13 @@ namespace EverPresent.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Shows the login page for the EverPresent app
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
-
-       
-        public ActionResult Student()
-        {
-            return View();
-        }
-
-        public ActionResult StSignIn()
-        {
-            return View();
-        }
-    
     }
 }
