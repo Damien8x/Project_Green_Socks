3fd2426eb0ea8a8ebaff4a317af7cbc9685f1638
damien <damien@DESKTOP-SE09OAK>
2018-06-03 12:53:04 -0700

    Added new project files and POST. We now accept Contact messages from users

--- EverPresent/EverPresent/Controllers/HomeController.cs
+++ EverPresent/EverPresent/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EverPresent.Models;
+using EverPresent.Backend;
 
 namespace EverPresent.Controllers
 {
@@ -16,5 +18,61 @@ namespace EverPresent.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// Shows a web form to submit a Message with user information
+        /// </summary>
+        /// <returns></returns>
+         public ActionResult Contact()
+        {
+            EverPresent.Models.ContactModel contact = new EverPresent.Models.ContactModel();
+            return View(contact);
+        }
+        
+        /// <summary>
+        /// Confirmation view for Contact. Re-displays message and information back to user
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public ActionResult ContactSuccess(ContactModel data)
+        {
+            return View(data);
+        }
+
+        /// <summary>
+        /// POST for Contact. Takes in form information and passes Model to ContactSuccess ActionResult
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Contact([Bind(Include=
+                                    "Id,"+
+                                    "Name,"+
+                                    "Email,"+
+                                    "Message," +
+                                    "")] ContactModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back for edit, with Error Message
+                return View(data);
+            }
+
+            if (data == null)
+            {
+                // Send to Error Page
+                return RedirectToAction("Error", new { route = "Home", action = "Error" });
+            }
+
+            if (string.IsNullOrEmpty(data.Id))
+            {
+                // Sind back for Edit
+                return View(data);
+            }
+
+
+            return RedirectToAction("ContactSuccess", "Home", data);
+        }
+
     }
 }
---
+++ EverPresent/EverPresent/Models/ContactModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using EverPresent.Models.Enums;
+using EverPresent.Backend;
+
+namespace EverPresent.Models
+{
+    public class ContactModel
+    {
+        /// <summary>
+        /// Model Id (unique key) tracks message Id for reference
+        /// </summary>
+        [Key]
+        [Display(Name = "Id", Description = "Message Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Used to store value of user's name for submitted messages
+        /// </summary>
+        [Display(Name = "Name", Description = "Sender's Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Store's user email for Admin response to user
+        /// </summary>
+        [Display(Name = "Email", Description = "Sender's Email")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Holds value of user's message on contact form
+        /// </summary>
+        [Display(Name = "Message", Description = "Sender's Message")]
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Default Constructor for ContactModel
+        /// </summary>
+        public ContactModel() {
+            this.Id = "1";
+            this.Name = "";
+            this.Email = "";
+            this.Message = "";
+        }
+
+        /// <summary>
+        /// ContactModel Constructor. Accepts parameters for all Model attributes
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="email"></param>
+        /// <param name="message"></param>
+        public ContactModel(string id, string name, string email, string message)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.Message = message;
+        }
+    }
+}
